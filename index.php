9548e4045b8c7d959558dd4fc0bd3f108e7cfbb4
nanna2018 <analuchi73@hotmail.es>
2018-05-02 09:01:39 +0200

    tercer repositorio

---
+++ index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+
+  </head>
+
+<header>
+ <nav class="navbar navbar-expand-lg navbar-link bg-link"><img src="imagen/open.jpg" alt="logo"height='80'>
+  <a class="navbar-brand" href="#">Furniture</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Workspaces<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Open Making</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Blog</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-link">Carro</button>
+        <button type="button" class="btn btn-link">Your dahboard</button>
+        <button type="button" class="btn btn-link">Join as a maker</button>
+    </div>
+    </form>
+  </div>
+ </nav>
+  <div class="contenedor">
+    <img id="hombre"src="imagen/open2.jpg" width="100%"/>
+        <div class="texto-encima"></div>
+        <div class="centrado"><h2>Furniture designed for<br> inspiring workplaces</h2><br>
+          <h4>We’re changing the way furniture is<br> made, by connecting customers to local<br> makers<h4>
+             <button type="button" class="btn btn-success">Shop Furniture</button>
+        </div>
+   </div>
+</headder>
+<body>
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="imagen/open3.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Len Desk</h5>
+      <h3 class="small">Generous four seat workstation</h3>
+    </div>
+</div>
+
+  <div class="card">
+    <img class="card-img-top" src="imagen/open4.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Linnea Bookshelf</h5>
+     <h3 class="small">Elegant shelving &amp; storage solution</h3>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="imagen/open5.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Johann Bench </h5>
+      <h3 class="small">2 person canteen and meeting bench</h3>
+      
+    </div>
+  </div>
+</div>
+<div class="contenedor">
+    <img id="hombre"src="imagen/open6.jpg" width="100%"/>
+        <div class="texto-encima"></div>
+        <div class="centrado"><h2>A global platform for local<br> making</h2><br>
+          <h4>Opendesk is an online marketplace that<br> hosts independently designed furniture<br> and connects its customers to local<br> makers around the world. Rather than<br> mass manufacturing and shipping<br> worldwide, we’re building a distributed<br> and ethical supply chain through a global<br> maker network.<h4>
+        </div>
+   </div>
+
+
+ 
+         <h6 id="s">HOW IT WORKS</h6>
+
+   <div id="columna"class="container">
+        <div class="row">
+                <div class="col-sm">
+                    <img="">
+                   <h3>Select designs</h3>
+                    <p>Browse and add designs to your basket or get in touch for help</p>
+               </div>
+                <div class="col-sm">
+                <h3>Request quotes</h3>
+                <p>Get quotes from local makers in your area within two business days</p>
+                </div>
+                <div class="col-sm">
+                    <h3>Choose your maker</h3>
+                    <p> Choose the local maker’s quote that works best for you</p>
+
+                </div>
+                <div class="col-sm">
+                    <h3>Receive your furniture</h3>
+                    <p>Get furniture delivered to your door in 2-4 weeks, direct from your maker</p>
+
+                </div>
+            </div>
+
+        </div>    
+    </div>
+    <div class="button">
+    <center><button type="button" class="btn btn-success btn-lg btn-log ">Success</button></center>
+</div>
+  <div class="video">
+    <div class="vid">
+       <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/215745438" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+    </div>
+   </div>
+   <div class="container" class="logo">
+  <div class="row">
+    <div class="col-sm" >
+     <img src="imagen/nike.jpg"height='100'>
+    </div>
+    <div class="col-sm">
+    <img src="imagen/we.jpg"height='100'>
+    </div>
+    <div class="col-sm">
+    <img src="imagen/Google.jpg"height='100'>
+    </div>
+    <div class="col-sm">
+    <img src="imagen/gree.jpg"height='100'>
+    </div>
+  </div>
+</div>
+<div class="contenedor">
+    <img id="padre"src="imagen/imag2.jpg" width="100%"/>
+        <div class="texto-encima"></div>
+        <div class="centrado"><h2>Inspiring workplaces for<br> amazing companies</h2><br>
+          <h4>We spend one third of our lives working,<br> so our work environments matter. These<br> companies bought Opendesk designs<br> from local makers because they’re<br> beautiful, made on-demand and built for<br> collaboration<br> makers<h4>
+             <button type="button" class="btn btn-success">Shop Furniture</button>
+        </div>
+   </div>
+</body>
+
+
+
+
+
+
+
+
+
+  
+   <!--Optional JavaScript-->
+    <!--jQuery first,the Pooper.js, the Booststrap JS-->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script src="app.js"></script>
+
+</body>
+
+</htm
